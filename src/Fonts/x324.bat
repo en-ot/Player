dfb962b7c8fb0ec0f09d909139be6639717b06b4
en-ot <andrey.mikhaylov.n@gmail.com>
2021-06-20 20:56:41 +0300

    added ( ) to font fixed icons

--- src/Fonts/x324.bat
+++ src/Fonts/x324.bat
@@ -1,5 +1,5 @@
 bin2hex.exe --i x324.vlw --o x324-2.h
 echo const uint8_t x324[] PROGMEM = { > x324.h
-cat x324-2.h >> x324.h
+type x324-2.h >> x324.h
 echo }; >> x324.h
-del x324-2.h
+rem copy /b x324-1.h+x324-2.h+x324-3.h x318.h /Y
