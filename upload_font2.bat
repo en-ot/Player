a07e38817db429f0e1170a4ad72b8cf3117dc0c7
en.ot <andrey.mikhaylov.n@gmail.com>
2021-08-08 21:07:37 +0300

    font partition 61

--- upload_font2.bat
+++ upload_font2.bat
@@ -1,1 +1,1 @@
-tools\esptool.exe --port COM49 --baud 2000000 write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x610000 data/x324.prt
+tools\esptool.exe --port COM49 --baud 2000000 write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x410000 data/x324.prt
