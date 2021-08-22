c24ab5884c8209abd2b7808dff12c68914e19c77
en.ot <andrey.mikhaylov.n@gmail.com>
2021-08-22 22:20:14 +0300

    OTA web update can be used in WIFI_MODE_AP

--- src/network.h
+++ src/network.h
@@ -6,9 +6,9 @@ bool network_address(char * buf, int len);
 void network_reconnect(bool ap);
 
 #define NETWORK_ENABLED 1
-//#define ARDUINO_OTA 1
+//#define ARDUINO_OTA 1     // small but ugly
 //#define HTTP_UPDATER 1
-//#define OTA_WEB_UPDATER 1
+#define OTA_WEB_UPDATER 1
 #define FTP_SERVER 1
 
 // NET_MODE_STA 0
